b838f5d13d5b9aaffe1340a5c24ba9ef6b66f11c
Blake Sticland <blakelists@yahoo.com.au>
2021-01-23 23:13:13 +1100

    questions added to the index.js file

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+const askQuestions = () => {
+    return  inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title"
+      },
+    //   {
+    //     type: "input",
+    //     name: "description"
+    //     message: "What is the purpose or function of the app?",
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "installation"
+    //     message: "How can the app be installed?",
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "usage"
+    //     message: "How can the app be used?",
+    //   },
+    //   {
+    //     type: "list",
+    //     name: "license"
+    //     message: "Which license would you like to apply to the app?",
+    //     choices: "",
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "contributing"
+    //     message: "How can users contribute to the app?",
+    //     choices: "",
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "github-username"
+    //     message: "What is your Github username?",
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "email-address"
+    //     message: "What is your email address?",
+    //   },
+    ]);
+  };
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
